e00517cadaed375b84a5dc5a11fd5eccde7b6e19
euclidesseg <euder0206@gmail.com>
2022-09-02 20:58:25 -0500

    02/09

---
+++ app/src/main/java/com/example/empresar/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.empresar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Ocultar la barra de menu por defecto
+        getSupportActionBar().hide();
+        //Libreria para el manejo de vistas (fragment)
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.Escenario,new SectionFragment()).commit();
+
+    }
+}
