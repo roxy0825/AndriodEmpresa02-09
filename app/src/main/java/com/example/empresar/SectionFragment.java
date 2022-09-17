dedd81d94b19f20e1b27010bef87ae1fdef94824
euclidesseg <euder0206@gmail.com>
2022-09-16 19:26:44 -0500

    1609

--- app/src/main/java/com/example/empresar/SectionFragment.java
+++ app/src/main/java/com/example/empresar/SectionFragment.java
@@ -10,11 +10,15 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
@@ -32,6 +36,7 @@ public class SectionFragment extends Fragment implements Response.Listener<JSONO
         // Inflate the layout for this fragment
        // return inflater.inflate(R.layout.fragment_section, container, false);
         View vista = inflater.inflate(R.layout.fragment_section,container,false);
+        rq = Volley.newRequestQueue(getContext());//requerimiento Volley
         jetcorreo =vista.findViewById(R.id.etcorreo);
         jetclave =vista.findViewById(R.id.etclave);
         jbtingresar =vista.findViewById(R.id.btingresar);
@@ -39,19 +44,36 @@ public class SectionFragment extends Fragment implements Response.Listener<JSONO
         jbtingresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Iniciar_sesion();
 
             }
         });
         return vista;
     }
-
-    @Override
-    public void onErrorResponse(VolleyError error) {
+    private void Iniciar_sesion(){
+        String correo,clave;
+        correo =jetcorreo.getText().toString();
+        clave =jetclave.getText().toString();
+        if (correo.isEmpty() || clave.isEmpty()){
+            Toast.makeText(getContext(), "Correo y Clave son requeridos", Toast.LENGTH_SHORT).show();
+            jetcorreo.requestFocus();
+        }
+        else {
+            String url = "http://172.16.60.24/WebServesRoxy/sesion.php?correo="+correo+"&clave="+clave;
+            jrq = new JsonObjectRequest(Request.Method.GET,url,null,this,this);
+            rq.add(jrq);
+        }
 
     }
 
     @Override
+    public void onErrorResponse(VolleyError error) {
+        Toast.makeText(getContext(), "Error en inicio de sesion", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
     public void onResponse(JSONObject response) {
+        Toast.makeText(getContext(), "Cargando sesion", Toast.LENGTH_SHORT).show();
 
     }
 }
